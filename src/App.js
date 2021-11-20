0daaf32faf2ee1d10e004cece29bd36df380e466
Jose-Miguel-FT <miguelfinol_48@hotmail.com>
2021-11-20 22:57:18 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -21,18 +21,64 @@ export default function App() {
     startDate: new Date(2021, 10, 19).toLocaleDateString(),
   };
 
-  const jsonData =
-    '[{"treatId": "01", "treatCourseId": "10", "type": "alpha", "category": "bravo", "name": "charlie", "startDate": ""},' +
-    '{"treatId": "02", "treatCourseId": "20", "type": "delta", "category": "echelon", "name": "foxtrot", "startDate": ""},' +
-    '{"treatId": "03", "treatCourseId": "30", "type": "golf", "category": "hotel", "name": "india", "startDate": ""},' +
-    '{"treatId": "04", "treatCourseId": "40", "type": "juliet", "category": "kilo", "name": "lima", "startDate": ""},' +
-    '{"treatId": "05", "treatCourseId": "50", "type": "mike", "category": "november", "name": "oscar", "startDate": ""}';
+  // const jsonData =
+  //   '[{"treatId": "01", "treatCourseId": "10", "type": "alpha", "category": "bravo", "name": "charlie", "startDate": ""},' +
+  //   '{"treatId": "02", "treatCourseId": "20", "type": "delta", "category": "echelon", "name": "foxtrot", "startDate": ""},' +
+  //   '{"treatId": "03", "treatCourseId": "30", "type": "golf", "category": "hotel", "name": "india", "startDate": ""},' +
+  //   '{"treatId": "04", "treatCourseId": "40", "type": "juliet", "category": "kilo", "name": "lima", "startDate": ""},' +
+  //   '{"treatId": "05", "treatCourseId": "50", "type": "mike", "category": "november", "name": "oscar", "startDate": ""}';
+
+  const normalData = [
+    {
+      treatId: '01',
+      treatCourseId: '10',
+      type: 'alpha',
+      category: 'bravo',
+      name: 'charlie',
+      startDate: '',
+    }, 
+    {
+      treatId: '02',
+      treatCourseId: '20',
+      type: 'delta',
+      category: 'echelon',
+      name: 'foxtrot',
+      startDate: '',
+    },
+    {
+      treatId: '03',
+      treatCourseId: '30',
+      type: 'golf',
+      category: 'hotel',
+      name: 'india',
+      startDate: '',
+    },
+    {
+      treatId: '04',
+      treatCourseId: '40',
+      type: 'juliet',
+      category: 'kilo',
+      name: 'lima',
+      startDate: '',
+    },
+    {
+      treatId: '05',
+      treatCourseId: '50',
+      type: 'mike',
+      category: 'november',
+      name: 'oscar',
+      startDate: '',
+    },
+  ];
+
+  const jsonData = JSON.stringify(normalData);
 
   return (
     <div>
       <h1>LH MEDICAL COMPANY</h1>
       <br />
       <MedicalTreatment medicalTreatment={medicalTreatment1} />
+      <br />
       <MedicalTreatmentList string={jsonData} />
     </div>
   );
--- src/components/MedicalTreatmentList.js
+++ src/components/MedicalTreatmentList.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 function MedicalTreatmentList(props) {
-
   //const medicalTreatments = JSON.parse(props.value);
   const productsJ = props.string;
+  const medicalTreatmentsArray = JSON.parse(productsJ);
 
+  const listMedicalTreats = medicalTreatmentsArray.map((item) => (
+    <li>
+      {''}
+      key = {item.treatId}, {item.treatCourseId}
+    </li>
+  ));
 
   return (
     <div>
       {productsJ}
+      <br />
+      <br />
+      {listMedicalTreats}
     </div>
-  )
+  );
 }
 
 export default MedicalTreatmentList;
